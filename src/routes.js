a7fa941e0457b218da8276e81884c15c6aef31d9
yourLogic01 <2010631170004@student.unsika.ac.id>
2025-05-22 14:44:48 +0700

    feat : add GET all note and GET note by ID method

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,21 @@
 /* eslint-disable linebreak-style */
-const { addNoteHandler } = require('./handler');
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler } = require('./handler');
 
 const routes = [
   {
     method: 'POST',
     path: '/notes',
     handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
   },
 ];
 module.exports = routes;
